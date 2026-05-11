b6f91f1a0f32040bd92dd622f51cf0091ee0694d
Fábio Naspolini <fabionaspolini@gmail.com>
2026-05-11 20:23:04 -0300

    style: kubernetes.sh

--- bash/aliases/kubernetes.sh
+++ bash/aliases/kubernetes.sh
@@ -1,6 +1,7 @@
 # Carrega autocomplete do kubectl
 source <(kubectl completion bash)
 
-# Associa a conclusão do kubectl ao alias 'k'
 alias k="kubectl"
+
+# Associar a conclusão do kubectl ao alias 'k'
 complete -o default -F __start_kubectl k
