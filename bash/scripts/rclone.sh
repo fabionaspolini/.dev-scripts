963ef57a5389e99c8f113b3adb517d0f77590529
Fábio Naspolini <fabionaspolini@gmail.com>
2026-05-17 22:52:43 -0300

    feat: add bisync alias and clean up rclone script
    - Introduced a new alias for rclone bisync with configuration options.
    - Removed commented-out management functions for clarity.
    - Cleaned up service log commands and adjusted formatting.

--- bash/scripts/rclone.sh
+++ bash/scripts/rclone.sh
@@ -1,33 +1,54 @@
-alias rclone-status="systemctl --user status googledrive-rclone.service --no-pager"
+#
+# Manual sync
+#
 
-alias rclone-show-journal-logs="journalctl --user -u googledrive-rclone.service -b --no-pager"
+alias rclone-bisync="rclone bisync \
+  googledrive: ~/GoogleDrive/ \
+  --progress \
+  --transfers 8 \
+  --fast-list \
+  --track-renames \
+  --log-level ERROR \
+  --log-file ~/.local/state/rclone/googledrive.log"
+
+#
+# Management commands
+#
+
+alias rclone-status="systemctl --user status googledrive-rclone.service --no-pager"
 alias rclone-show-service-logs="tail -n 20 ~/.local/state/rclone/googledrive.log"
 alias rclone-open-service-logs="kate ~/.local/state/rclone/googledrive.log"
 
-rclone-stop() {
-  systemctl --user stop googledrive-rclone.service
-  rclone-status
-}
-
-rclone-start() {
-  systemctl --user daemon-reload
-  systemctl --user start googledrive-rclone.service
-  rclone-status
-}
-
-rclone-restart() {
-  systemctl --user daemon-reload
-  systemctl --user restart googledrive-rclone.service
-  rclone-status
-}
-
 rclone-selfupdate() {
-  echo "Stoping service"
-  systemctl --user stop googledrive-rclone.service
+#  echo "Stoping service"
+#  systemctl --user stop googledrive-rclone.service
   
   echo "Self update"
   sudo rclone selfupdate
   
-  echo "Starting service"
-  rclone-start
+#  echo "Starting service"
+#  rclone-start
 }
+
+#
+# Only to VFS as a service mode
+#
+
+#alias rclone-show-journal-logs="journalctl --user -u googledrive-rclone.service -b --no-pager"
+#
+#rclone-stop() {
+#  systemctl --user stop googledrive-rclone.service
+#  rclone-status
+#}
+#
+#rclone-start() {
+#  systemctl --user daemon-reload
+#  systemctl --user start googledrive-rclone.service
+#  rclone-status
+#}
+#
+#rclone-restart() {
+#  systemctl --user daemon-reload
+#  systemctl --user restart googledrive-rclone.service
+#  rclone-status
+#}
