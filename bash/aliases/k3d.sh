1b5e7ec0f251c1274b67939fab72970bac0e2701
Fábio Naspolini <fabionaspolini@gmail.com>
2026-07-06 16:51:08 -0300

    fix: correct k3d alias commands for starting and stopping the cluster

--- bash/aliases/k3d.sh
+++ bash/aliases/k3d.sh
@@ -1,2 +1,2 @@
-alias k3d-start="k3d cluster stop my-cluster"
-alias k3d-stop="k3d cluster start my-cluster"
+alias k3d-start="k3d cluster start my-cluster"
+alias k3d-stop="k3d cluster stop my-cluster"
