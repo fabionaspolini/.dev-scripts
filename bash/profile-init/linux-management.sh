a9ef6f77e6de4c324926e59663bb6adb8dcd99a8
Fábio Naspolini <fabionaspolini@gmail.com>
2026-08-03 11:04:04 -0300

    refactor: enhance package upgrade prompts with color coding

--- bash/profile-init/linux-management.sh
+++ bash/profile-init/linux-management.sh
@@ -3,22 +3,22 @@ alias fix-keyboard-ibus-errors="ibus restart"
 upgrade-all-packages() {
     echo "Upgrading all packages..."
 
-    echo -e "\033[1mPress any key to upgrade packages using dnf...\033[0m"
+    echo -e "\033[1mPress any key to upgrade packages using \033[33mdnf\033[0m\033[1m...\033[0m"
     read -n1 -s
     sudo dnf upgrade --refresh
 
     echo "----------------------------------------"
-    echo -e "\033[1mPress any key to upgrade packages using pkcon...\033[0m"
+    echo -e "\033[1mPress any key to upgrade packages using \033[33mpkcon\033[0m\033[1m...\033[0m"
     read -n1 -s
     pkcon refresh && pkcon update
 
     echo "----------------------------------------"
-    echo -e "\033[1mPress any key to upgrade packages using flatpak...\033[0m"
+    echo -e "\033[1mPress any key to upgrade packages using \033[33mflatpak\033[0m\033[1m...\033[0m"
     read -n1 -s
     flatpak update
 
     echo "----------------------------------------"
-    echo -e "\033[1mPress any key to upgrade packages using snap...\033[0m"
+    echo -e "\033[1mPress any key to upgrade packages using \033[33msnap\033[0m\033[1m...\033[0m"
     read -n1 -s
     sudo snap refresh
 
