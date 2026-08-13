b46d18e780d9452e340788013c191818d38213b7
Fábio Naspolini <fabionaspolini@gmail.com>
2026-08-13 17:01:25 -0300

    fix: reorder unset commands in clear-ibus.sh for clarity and consistency

--- bash/system-triggers/on-login/before-session-start/clear-ibus.sh
+++ bash/system-triggers/on-login/before-session-start/clear-ibus.sh
@@ -1,5 +1,10 @@
 
 # Remover modulos de input legados para o IBUS Wayland
-unset QT_IM_MODULE
 unset GTK_IM_MODULE
+unset QT_IM_MODULE
 unset XMODIFIERS
+
+export LC_CTYPE=pt_BR.UTF-8
+
+# export GTK_IM_MODULE=cedilla
+# export QT_IM_MODULE=cedilla
