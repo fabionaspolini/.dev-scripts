8e1b86c3b3447d76a6a71fe37b7a7734fafd927c
Fábio Naspolini <fabionaspolini@gmail.com>
2026-08-25 22:49:39 -0300

    fix: comment out snap upgrade section in upgrade-all-packages function

--- bash/profile-init/linux-management.sh
+++ bash/profile-init/linux-management.sh
@@ -17,10 +17,10 @@ upgrade-all-packages() {
     read -n1 -s
     flatpak update
 
-    echo "----------------------------------------"
-    echo -e "\033[1mPress any key to upgrade packages using \033[33msnap\033[0m\033[1m...\033[0m"
-    read -n1 -s
-    sudo snap refresh
+    # echo "----------------------------------------"
+    # echo -e "\033[1mPress any key to upgrade packages using \033[33msnap\033[0m\033[1m...\033[0m"
+    # read -n1 -s
+    # sudo snap refresh
 
     echo "----------------------------------------"
     echo "Finishing package upgrades."
